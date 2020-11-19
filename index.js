a9ac01151ff0409e47aa7733533cdf957de2b694
Bálint Kozák <43552747+kozakbalint@users.noreply.github.com>
2020-11-19 09:06:31 +0100

    Form finished and js started.

---
+++ index.js
@@ -0,0 +1,19 @@
+maxPoint = 130
+
+function GetValues()
+{ 
+//tartalom
+fogIsm = document.getElementById("fogIsm").value
+fogDef = document.getElementById("fogDef").value
+tarKif = document.getElementById("tarKif").value
+elAlk = document.getElementById("elAlk").value
+oszErt = document.getElementById("oszErt").value
+//felepeites
+idBe = document.getElementById("idBe").value
+feEp = document.getElementById("feEp").value
+//eloadas
+elAd = document.getElementById("elAd").value
+szaNye = document.getElementById("szaNye").value
+//irasbeli
+iras = document.getElementById("iras").value
+}
